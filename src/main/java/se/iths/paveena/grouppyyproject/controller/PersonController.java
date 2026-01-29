dcf5e70e462e6c73e997db7e4f8cbd78c95e753f
paveena-naebsuwan <paveena.naebsuwan@gmail.com>
2026-01-29 23:37:49 +0100

    Smal changes

--- src/main/java/se/iths/paveena/grouppyyproject/controller/PersonController.java
+++ src/main/java/se/iths/paveena/grouppyyproject/controller/PersonController.java
@@ -3,11 +3,13 @@ package se.iths.paveena.grouppyyproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import se.iths.paveena.grouppyyproject.model.Person;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class PersonController {
 
     @GetMapping("/person")
