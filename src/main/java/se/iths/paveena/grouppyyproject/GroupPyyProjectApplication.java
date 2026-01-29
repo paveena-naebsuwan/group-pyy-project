79fc0684d590702ce3272c3da88522531c74ad85
paveena-naebsuwan <paveena.naebsuwan@gmail.com>
2026-01-29 15:30:10 +0100

    First commit

---
+++ src/main/java/se/iths/paveena/grouppyyproject/GroupPyyProjectApplication.java
@@ -0,0 +1,13 @@
+package se.iths.paveena.grouppyyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupPyyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupPyyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/paveena/grouppyyproject/GroupPyyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.paveena.grouppyyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupPyyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
