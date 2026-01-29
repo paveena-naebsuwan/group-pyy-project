6846e42ef904f564a05e5400616fedc9654d9f8c
paveena-naebsuwan <paveena.naebsuwan@gmail.com>
2026-01-29 15:49:22 +0100

    Added Homecontroller

---
+++ src/main/java/se/iths/paveena/grouppyyproject/controller/HomeController.java
@@ -0,0 +1,18 @@
+package se.iths.paveena.grouppyyproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String welcome(Model model){
+        String greeting = "Välkommen";
+        model.addAttribute("greeting", greeting);
+        return "welcome";
+    }
+}
