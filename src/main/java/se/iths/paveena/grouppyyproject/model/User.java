76b7104bea2196f160de8459dccd5a90527cc8e0
paveena-naebsuwan <paveena.naebsuwan@gmail.com>
2026-01-29 21:30:25 +0100

    Moved constructor to the top

--- src/main/java/se/iths/paveena/grouppyyproject/model/User.java
+++ src/main/java/se/iths/paveena/grouppyyproject/model/User.java
@@ -5,6 +5,15 @@ public class User {
     private String name;
     private String email;
 
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
     public int getId() {
         return id;
     }
@@ -26,15 +35,6 @@ public class User {
     }
 
     public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public User() {
-    }
-
-    public User(int id, String name, String email) {
-        this.id = id;
-        this.name = name;
         this.email = email;
     }
 }
