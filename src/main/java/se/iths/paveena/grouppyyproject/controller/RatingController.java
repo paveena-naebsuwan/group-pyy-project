10d3c132a2f81acf0a3a27a00bf1686b660a4080
ynsuldg <yns.uldg@hotmail.com>
2026-01-29 22:45:45 +0100

    initial commit

---
+++ src/main/java/se/iths/paveena/grouppyyproject/controller/RatingController.java
@@ -0,0 +1,33 @@
+package se.iths.paveena.grouppyyproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import se.iths.paveena.grouppyyproject.model.Rating;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ratings")
+public class RatingController {
+
+    @GetMapping
+    public String getRatings(Model model) {
+
+        List<Rating> ratings = List.of(
+                new Rating(1L, "Yunus", 5.0),
+                new Rating(2L, "Yunus-can", 3.8),
+                new Rating(3L, "Paveena", 3.4)
+        );
+        model.addAttribute("ratings", ratings);
+        return "ratings";
+    }
+
+    @PostMapping
+    public String createRating(Model model) {
+        model.addAttribute("message", "Betyg lagt!");
+        return "ratings";
+    }
+}
