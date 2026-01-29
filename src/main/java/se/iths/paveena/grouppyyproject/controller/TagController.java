3c6d92e0a9f3d44f1cccad4f7f129af3dbe6a905
ynsuldg <yns.uldg@hotmail.com>
2026-01-29 23:09:37 +0100

    initial commit

--- src/main/java/se/iths/paveena/grouppyyproject/controller/TagController.java
+++ src/main/java/se/iths/paveena/grouppyyproject/controller/TagController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.iths.paveena.grouppyyproject.model.Tag;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -17,9 +18,9 @@ public class TagController {
     public String tags(Model model) {
 
         List<Tag> tags = List.of(
-                new Tag(1L, "Coding"),
-                new Tag(1L, "JavaCoding"),
-                new Tag(3L, "LearningToCode")
+                new Tag(1L, "Coding", LocalDateTime.now()),
+                new Tag(1L, "JavaCoding", LocalDateTime.now()),
+                new Tag(3L, "LearningToCode", LocalDateTime.now())
         );
         model.addAttribute("tags", tags);
         return "tags";
--- src/main/java/se/iths/paveena/grouppyyproject/model/Tag.java
+++ src/main/java/se/iths/paveena/grouppyyproject/model/Tag.java
@@ -1,16 +1,20 @@
 package se.iths.paveena.grouppyyproject.model;
 
+import java.time.LocalDateTime;
+
 public class Tag {
 
     private Long id;
     private String name;
+    private LocalDateTime createdAt;
 
     public Tag() {
     }
 
-    public Tag(Long id, String name) {
+    public Tag(Long id, String name, LocalDateTime createdAt) {
         this.id = id;
         this.name = name;
+        this.createdAt = createdAt;
     }
 
     public Long getId() {
@@ -27,5 +31,13 @@ public class Tag {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
