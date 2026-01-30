1e3ffe25818dcc341de90ae27ed82dcd09cc40f9
Yunus-CanYucel <yunuscan.ycel@iths.se>
2026-01-30 11:35:04 +0100

    Made Que

---
+++ src/main/java/se/iths/paveena/grouppyyproject/model/Que.java
@@ -0,0 +1,17 @@
+package se.iths.paveena.grouppyyproject.model;
+
+public class Que {
+    private String wait;
+
+    public Que(String wait) {
+        this.wait = wait;
+    }
+
+
+    public String getWait() {
+        return wait;
+    }
+    public void setWait(String wait) {
+        this.wait = wait;
+    }
+}
